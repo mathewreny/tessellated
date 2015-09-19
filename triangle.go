c49a6a66e7647eca243af8b6382a19fbb3bbe9b6
MATHEW RENY <mhreny@gmail.com>
2015-09-19 01:47:10 -0500

    Untested triangle.go initial commmit

---
+++ triangle.go
@@ -0,0 +1,94 @@
+package tessellated
+
+import (
+	"fmt"
+	"io"
+	"math"
+	"math/rand"
+	"time"
+)
+
+const (
+	base   = 100.0
+	height = base * 1.732050808 * 0.5 // base * (sqrt(3)/2)
+	radius = base * 0.35              // should be strictly smaller than (base * 0.5)
+	circle = 2.0 * math.Pi
+
+	ystart = 0.0 - height
+	xo     = 0.0 - base     // Where every odd row starts in the x direction.
+	xe     = 0.0 - base*0.5 // Where every even row starts in the x direction.
+
+	maxopacity = 0.15
+
+	svgprefix   = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="%dpx" height="%dpx" viewBox="0 0 %d %d" zoomAndPan="disable">`
+	svgtriangle = `<polygon points="%v %v %v" style="fill:#090909;opacity:%0.2f" />`
+)
+
+var random *math.Rand = rand.New(rand.NewSource(time.Now().Unix()))
+
+type Rect struct {
+	Width, Height float64
+}
+
+type point struct {
+	X, Y float64
+}
+
+func (p *point) randomize() {
+	hypot := radius * random.Float64()
+	theta := circle * random.Float64()
+	p.X += hypot * math.Cos(theta)
+	p.Y += hypot * math.Sin(theta)
+}
+
+func (p *point) GoString() string {
+	return p.String()
+}
+
+func (p *point) String() string {
+	return fmt.Sprintf("%0.2f,%0.2f", p.X, p.Y)
+}
+
+type row []point
+
+func reset(x, y float64, r row) {
+	n := len(r)
+	for i := 0; i < n; x, i += base, 1 {
+		p := &r[i]
+		p.X = x
+		p.Y = y
+		p.randomize()
+	}
+}
+
+func Triangle(size Rect, w io.Writer) {
+	fmt.Fprintf(w, svgprefix, size.Width, size.Height, size.Width, size.Height)
+	nr := 3 + int(math.Ceil(size.Height/height)) // Number of rows of points
+	npo := 3 + int(math.Ceil(size.Width/base))   // Number of points in an odd row
+	npe := 2 + int(math.Ceil(size.Width/base))   // Number of points in an even row
+
+	// Allocates even and odd rows only once per function call! Could use a sync.Pool
+	o := make(row, npo)
+	reset(xo, ystart, o)
+	e := make(row, npe)
+
+	// Construct the triangles
+	for y, i := 0.0, 1; i < nr; y, i += height, 1 {
+		if 0 == i%2 {
+			// Odd row
+			reset(xo, y, o)
+			for j := 0; j < npe; j++ {
+				opacity := maxopacity * random.Float64()
+				fmt.Fprintf(w, svgtriangle, e[j], o[j], o[j+1], opacity)
+			}
+		} else {
+			// Even row
+			reset(xe, y, &e)
+			for j := 0; j < npe; j++ {
+				opacity := maxopacity * random.Float64()
+				fmt.Fprintf(w, svgtriangle, o[j], o[j+1], e[j], opacity)
+			}
+		}
+	}
+	fmt.Fprint(w, svgsuffix)
+}
